7e6bdff9f1ed14223ef372337f161fa51137a5bb
zinebadi <45413450+zinebadi@users.noreply.github.com>
2019-01-04 11:19:49 +0800

    Update helpers.c

--- helpers.c
+++ helpers.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
-int is_rest(string s)
+bool is_rest(string s)
 {
-if (
+if (strcmp(s, " "))
+    {
+      return false;
+    }
+else 
+    {
+      return true;
+    }
 }
+//Duration function
+
+int duration(string fraction);
+
+int x = atoi("fraction[0]");
+int y = atoi("fraction[2]");
+int dur = (8/y)*x
+printf("the duraction of this note is %i"), dur);
+
+// frequency function 
+int frequency(string note);
+
+if (note == 3)
+{
+  int octave = atoi(note[3]);
+  if (octave == 3)
+      {
+        
+      }
+  else if (octave == 4)
+      {
+        
+      }
+  else if (octave == 5)
+      {
+        
+      }
+}
+
+if (note == 2}
+    { 
+      int octave = atoi(note[2]);
+      if (octave == 3)
+      {
+        
+      }
+      else if (octave == 4)
+      {
+        
+      }
+      else if (octave == 5)
+      {
+        
+      }
+     
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
